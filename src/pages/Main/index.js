6aedefbd92036c2a9b3c81f086f5ce3689bc4af0
Carlos Schuenck <carlosschuenck@hotmail.com>
2019-05-04 14:19:23 -0300

    Adicionando funcionalidade para deletar box e adicionando bootstrap

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,8 @@ import api from "../../services/api";
 import "./styles.css";
 import "../Box/styles.css";
 import logo from "../../assets/logo.svg";
-import { MdFolderOpen, MdOpenInBrowser } from "react-icons/md";
+import { MdOpenInBrowser, MdDelete } from "react-icons/md";
+import { Button, Row, Col, Tooltip, OverlayTrigger } from "react-bootstrap";
 
 export default class Main extends Component {
   state = {
@@ -23,6 +24,10 @@ export default class Main extends Component {
 
   openBox = id => {
     this.props.history.push(`/box/${id}`);
+  };
+
+  deleteBox = id => {
+    api.delete(`/boxes/${id}`).then(() => this.findAllBoxes());
   };
 
   handleSubmit = async event => {
@@ -56,13 +61,45 @@ export default class Main extends Component {
             {this.state.boxes &&
               this.state.boxes.map(box => (
                 <li key={box._id}>
-                  <strong>{box.title}</strong>
+                  <Row className="row-table">
+                    <Col>
+                      <strong>{box.title}</strong>
+                    </Col>
+                    <Col md={1}>
+                      <OverlayTrigger
+                        overlay={
+                          <Tooltip id="tooltip-top">
+                            Clique para acessar este box!
+                          </Tooltip>
+                        }
+                      >
+                        <Button variant="info" className="action-button">
+                          <MdOpenInBrowser
+                            size={20}
+                            onClick={() => this.openBox(box._id)}
+                          />
+                        </Button>
+                      </OverlayTrigger>
+                    </Col>
 
-                  <MdOpenInBrowser
-                    className="click"
-                    size={24}
-                    onClick={() => this.openBox(box._id)}
-                  />
+                    <Col md={1}>
+                      <OverlayTrigger
+                        overlay={
+                          <Tooltip id="tooltip-top">
+                            Clique para deletar este box!
+                          </Tooltip>
+                        }
+                      >
+                        <Button
+                          variant="danger"
+                          className="action-button"
+                          onClick={() => this.deleteBox(box._id)}
+                        >
+                          <MdDelete size={20} />
+                        </Button>
+                      </OverlayTrigger>
+                    </Col>
+                  </Row>
                 </li>
               ))}
           </ul>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://dropbox-backend-omnistack.herokuapp.com'
+  // baseURL: 'https://dropbox-backend-omnistack.herokuapp.com',
+  baseURL: "http://localhost:3333/"
 });
 
 export default api;
