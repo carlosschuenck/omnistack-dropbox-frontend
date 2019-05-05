d330097bb62cc326b922ea5855203dccae8ae904
Carlos Schuenck <carlosschuenck@hotmail.com>
2019-05-05 20:10:42 -0300

    Modificações no layout

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,65 +1,78 @@
-import React, { Component } from 'react';
-import './styles.css';
-import { MdInsertDriveFile } from 'react-icons/md';
-import logo from '../../assets/logo.svg';
-import api from '../../services/api';
-import { distanceInWords } from 'date-fns';
-import pt from 'date-fns/locale/pt';
-import Dropzone from 'react-dropzone';
-import socket from 'socket.io-client';
+import React, { Component } from "react";
+import "./styles.css";
+import { MdInsertDriveFile, MdArrowBack } from "react-icons/md";
+import logo from "../../assets/logo.svg";
+import api from "../../services/api";
+import { distanceInWords } from "date-fns";
+import pt from "date-fns/locale/pt";
+import Dropzone from "react-dropzone";
+import socket from "socket.io-client";
+import { Button, Col } from "react-bootstrap";
 
 export default class Box extends Component {
   state = {
     box: {}
-  }
+  };
 
-  async componentDidMount(){
+  async componentDidMount() {
     this.subscribeToNewFiles();
 
     const box = this.props.match.params.id;
-    const response = await api.get(`/boxes/${box}`)
-    this.setState({ box: response.data});
+    const response = await api.get(`/boxes/${box}`);
+    this.setState({ box: response.data });
   }
 
-  subscribeToNewFiles = () =>{
+  returnHomePage = () => {
+    this.props.history.push("/");
+  };
+  subscribeToNewFiles = () => {
     const box = this.props.match.params.id;
-    const io = socket('https://dropbox-backend-omnistack.herokuapp.com');
-    
-    io.emit('connectRoom', box);
+    const io = socket("https://dropbox-backend-omnistack.herokuapp.com");
 
-    io.on('file', data => {
+    io.emit("connectRoom", box);
+
+    io.on("file", data => {
       console.log("data", data);
-      this.setState(
-        { 
-          box: { ...this.state.box, 
-          files: [data, ...this.state.box.files]}
-        })
-    })
-  }
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box.files] }
+      });
+    });
+  };
 
-  handleUpload = (files) => {
+  handleUpload = files => {
     files.forEach(file => {
       const data = new FormData();
       const box = this.props.match.params.id;
 
-      data.append('file', file);
+      data.append("file", file);
 
       api.post(`/boxes/${box}/files`, data);
     });
   };
-  
+
   render() {
     return (
       <div id="box-container">
         <header>
+          <Col sm={3} md={4} id="back-button-aling">
+            <Button
+              size="sm"
+              id="buttom"
+              onClick={() => {
+                this.returnHomePage();
+              }}
+            >
+              <MdArrowBack size={24} />
+            </Button>
+          </Col>
           <img src={logo} alt="" />
           <h1>{this.state.box.title}</h1>
         </header>
 
         <Dropzone onDropAccepted={this.handleUpload}>
-          {({getRootProps, getInputProps}) => (
-            <div className="upload" { ... getRootProps()}>
-              <input { ... getInputProps()} />
+          {({ getRootProps, getInputProps }) => (
+            <div className="upload" {...getRootProps()}>
+              <input {...getInputProps()} />
 
               <p>Arraste arquivos ou clique aqui</p>
             </div>
@@ -67,16 +80,25 @@ export default class Box extends Component {
         </Dropzone>
 
         <ul>
-          { this.state.box.files && this.state.box.files.map(file => (
-            <li key={file._id}>
-              <a className="fileInfo" href={file.url} target="_blank" rel="noopener noreferrer">
-                <MdInsertDriveFile size={24} color="#A5Cfff" />
-                <strong>{file.title}</strong>
-              </a>
+          {this.state.box.files &&
+            this.state.box.files.map(file => (
+              <li key={file._id}>
+                <a
+                  className="fileInfo"
+                  href={file.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <MdInsertDriveFile size={24} color="#A5Cfff" />
+                  <strong>{file.title}</strong>
+                </a>
 
-              <span>Há {distanceInWords(file.createdAt, new Date(), { locale: pt})}</span>
-            </li>
-           )) }
+                <span>
+                  Há{" "}
+                  {distanceInWords(file.createdAt, new Date(), { locale: pt })}
+                </span>
+              </li>
+            ))}
         </ul>
       </div>
     );
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 import "./styles.css";
-import "../Box/styles.css";
 import logo from "../../assets/logo.svg";
 import { MdOpenInBrowser, MdDelete } from "react-icons/md";
-import { Button, Row, Col, Tooltip, OverlayTrigger } from "react-bootstrap";
+import {
+  Button,
+  Row,
+  Col,
+  Tooltip,
+  OverlayTrigger,
+  Table
+} from "react-bootstrap";
 
 export default class Main extends Component {
   state = {
@@ -49,60 +55,74 @@ export default class Main extends Component {
           <form onSubmit={this.handleSubmit}>
             <img src={logo} alt="" />
             <input
-              placeholder="Criar um box"
+              placeholder="Escreva o nome da box..."
               value={this.state.newBox}
               onChange={this.handleInputChange}
             />
             <button type="submit">Criar</button>
           </form>
         </div>
-        <div id="box-container">
-          <ul>
-            {this.state.boxes &&
-              this.state.boxes.map(box => (
-                <li key={box._id}>
-                  <Row className="row-table">
-                    <Col>
+        <div className="table-style">
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>Nome do Box</th>
+                <th className="action-column">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.boxes &&
+                this.state.boxes.map(box => (
+                  <tr key={box._id}>
+                    <td>
                       <strong>{box.title}</strong>
-                    </Col>
-                    <Col md={1}>
-                      <OverlayTrigger
-                        overlay={
-                          <Tooltip id="tooltip-top">
-                            Clique para acessar este box!
-                          </Tooltip>
-                        }
-                      >
-                        <Button variant="info" className="action-button">
-                          <MdOpenInBrowser
-                            size={20}
-                            onClick={() => this.openBox(box._id)}
-                          />
-                        </Button>
-                      </OverlayTrigger>
-                    </Col>
-
-                    <Col md={1}>
-                      <OverlayTrigger
-                        overlay={
-                          <Tooltip id="tooltip-top">
-                            Clique para deletar este box!
-                          </Tooltip>
-                        }
-                      >
-                        <Button
-                          variant="danger"
-                          className="action-button"
-                          onClick={() => this.deleteBox(box._id)}
-                        >
-                          <MdDelete size={20} />
-                        </Button>
-                      </OverlayTrigger>
-                    </Col>
-                  </Row>
-                </li>
-              ))}
-          </ul>
+                    </td>
+                    <td>
+                      <Row>
+                        <Col md={6}>
+                          <OverlayTrigger
+                            overlay={
+                              <Tooltip id="tooltip-top">
+                                Clique para acessar este box
+                              </Tooltip>
+                            }
+                          >
+                            <Button
+                              variant="info"
+                              className="action-button"
+                              size="sm"
+                            >
+                              <MdOpenInBrowser
+                                size={20}
+                                onClick={() => this.openBox(box._id)}
+                              />
+                            </Button>
+                          </OverlayTrigger>
+                        </Col>
+                        <Col md={6}>
+                          <OverlayTrigger
+                            overlay={
+                              <Tooltip id="tooltip-top">
+                                Clique para deletar este box
+                              </Tooltip>
+                            }
+                          >
+                            <Button
+                              variant="danger"
+                              className="action-button"
+                              size="sm"
+                              onClick={() => this.deleteBox(box._id)}
+                            >
+                              <MdDelete size={20} />
+                            </Button>
+                          </OverlayTrigger>
+                        </Col>
+                      </Row>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
         </div>
       </div>
     );
